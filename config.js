3862efa93ad2f723b348793ae38fe5cb58e42496
Naeem <166661007+naeemsomg@users.noreply.github.com>
2024-04-16 17:49:48 -0400

    Update config.js for password protection

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     naeem: 'naeem',
-    james: 'james'
   },
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
+  routes: false, // Change this to false if you just want to host a bare server.
+  local: false, // Change this to false to disable local assets.
 }
 export default config
